34762f4f0403ac922c24f49f7371cebe8a8a30cd
Sabrin <cse_2012020251@lus.ac.bd>
2023-07-02 00:14:07 +0600

    commit

--- server/database/db.js
+++ server/database/db.js
@@ -2,11 +2,8 @@
 import mongoose from "mongoose";
 
 export const Connection = async (username, password) => {
-    // const URL = `mongodb://${username}:${password}@ac-i6oeydc-shard-00-00.yemyogm.mongodb.net:27017,ac-i6oeydc-shard-00-01.yemyogm.mongodb.net:27017,ac-i6oeydc-shard-00-02.yemyogm.mongodb.net:27017/4th-project-genZ?ssl=true&replicaSet=atlas-i70bk3-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
-    const URL = `mongodb://${username}:${password}@4th-project-genz.yemyogm.mongodb.net`;
-
-
+       const URL = `mongodb+srv://${username}:${password}@4th-project-genz.tpbyne8.mongodb.net/`;
 
     try{
         mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
