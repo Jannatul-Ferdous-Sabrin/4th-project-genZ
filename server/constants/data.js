c9a5ad8d2640a8231d6658dafb25e0c67755a48a
Sabrin <cse_2012020251@lus.ac.bd>
2023-07-02 13:52:35 +0600

    commit

--- server/constants/data.js
+++ server/constants/data.js
@@ -2,15 +2,15 @@
 export const data = {
   users: [
     {
-      name: 'Noah',
+      name: 'Fered',
       email: 'delray@gmail.com',
       password: 'password123',
       phone: '1234567890',
       address: '123 Uposhohor',
     },
     {
-      name: 'Taylor',
-      email: 'william@gmail.com',
+      name: 'Lana',
+      email: 'ayja@gmail.com',
       password: 'pass123',
       phone: '9876543210',
       address: '456 housing',
