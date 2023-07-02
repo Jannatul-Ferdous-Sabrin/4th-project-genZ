cfa7889ff15d9d16f43a6801e4dbd043582da06f
Sabrin <cse_2012020251@lus.ac.bd>
2023-07-02 22:48:23 +0600

    login-reg form updated

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
-
-import { AppBar, Toolbar, styled } from '@mui/material';
-import Box from '@mui/material/Box';
+import { AppBar, Toolbar, styled, Typography, Box } from '@mui/material';
+import Search from './Search';
+import CustomerButtons from './CustomerButtons';
 
 const StyledHeader = styled(AppBar)({
   background: '#2874f0',
@@ -8,24 +8,30 @@ const StyledHeader = styled(AppBar)({
 });
 
 const Component = styled(Box)({
-    marginLeft: '12%',
-  });
-  
+  marginLeft: '12%',
+  lineheight:0,
+});
+
+
+const CustomerButtonsWrapper = styled(Box)`
+  margin: 0 5% 0 auto;
+`;
+
 const Header = () => {
-const logoURL = 'https://i.ibb.co/fv5ZZQd/profile.png';
+  const logoURL = 'https://i.ibb.co/fv5ZZQd/profile.png  ';
   return (
     <StyledHeader>
       <Toolbar>
         <Component>
-          <img src={logoURL} alt="logo" style={{width:'250px' }} />
-          <Box>
-
-          </Box>
+          <img src={logoURL} alt="logo" style={{ width: 200 }} />
         </Component>
+        <Search />
+        <CustomerButtonsWrapper>
+          <CustomerButtons />
+        </CustomerButtonsWrapper>
       </Toolbar>
     </StyledHeader>
   );
 };
 
 export default Header;
-
