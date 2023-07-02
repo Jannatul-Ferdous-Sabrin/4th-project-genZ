11a311ea326848cb8e9d2da2c534c7d1f554f316
Sabrin <cse_2012020251@lus.ac.bd>
2023-07-02 13:03:24 +0600

    user schema added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import Connection from './database/db.js';
 import DefaultData from './default.js';
 
+
+
 const app = express();
 
 dotenv.config();
@@ -10,7 +12,7 @@ const PORT = 8080;
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-Connection(USERNAME, PASSWORD);
+Connection(USERNAME, PASSWORD)
 
 app.get("/", (req,res) => {
   res.send("welcome to home page ");
