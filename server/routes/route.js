25bfb39de3555b413b0dd99ddffbea141389b5fa
Sabrin <cse_2012020251@lus.ac.bd>
2023-07-04 21:10:24 +0600

    login commit

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,13 @@
+
+
+
+
 import express from 'express';
+import { userSignup, userLogin } from '../controller/user-controller.js';
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-
-});
+router.post('/signup' , userSignup);
+router.post('/login' , userLogin);
 
 export default router;
